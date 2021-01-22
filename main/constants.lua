78b78cc34c01f0c0beea4a0d6c47c346b249971d
Dave Jorgenson <jorgenson.dave@gmail.com>
2021-01-22 18:37:55 -0500

    initial

---
+++ main/constants.lua
@@ -0,0 +1,10 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+M.defaultRegion = "battlefieldProxy"
+M.space1Region = "space1Proxy"
+
+return M
