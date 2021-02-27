1eaeb6a30b12fad1fc3c8cbff96429f2b0b3b2f6
Dave Jorgenson <jorgenson.dave@gmail.com>
2021-02-27 15:26:06 -0500

    gui, commands, bugs
    Added GUI with working commands to spawn miners and fighters, sort of fixed a bug with laser blast creation

--- main/constants.lua
+++ main/constants.lua
@@ -6,5 +6,9 @@ local M = {}
 
 M.defaultRegion = "battlefieldProxy"
 M.space1Region = "space1Proxy"
+M.cmdBuildMiner = hash("build-miner")
+M.cmdBuildSabre = hash("build-sabre")
+M.cmdRepairStation = hash("repair-station")
+M.GameRunnerScript = "/GameRunner#space"
 
 return M
